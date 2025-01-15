4e8e2bd59d78954764de1c7902b112f0129c5170
WOWOW0wOw <jongseok8529@gmail.com>
2025-01-15 17:37:29 +0900

    fead : add controller package

--- level9/src/main/_Main.java
+++ level9/src/main/_Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import controller.Controller;
+
 public class _Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("안녕하숩네까");
+		Controller con = new Controller();
+		
 	}
 
 }
